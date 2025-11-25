e255efb67e1883b2ec4486458f4fde3506ea2456
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-25 08:01:57 -0300

    feat: add screen retriever and window manager plugins for enhanced functionality across platforms

--- RetroGate.UI/macos/Flutter/GeneratedPluginRegistrant.swift
+++ RetroGate.UI/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import gamepads_darwin
+import screen_retriever_macos
+import shared_preferences_foundation
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GamepadsDarwinPlugin.register(with: registry.registrar(forPlugin: "GamepadsDarwinPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
