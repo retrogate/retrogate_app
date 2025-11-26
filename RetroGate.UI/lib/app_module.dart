60b9ad35dd07c0ebdd81dafce291fc84e9d98f48
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-25 23:13:21 -0300

    feat: enhance AppModule with Installer dependencies and global bindings

--- RetroGate.UI/lib/app_module.dart
+++ RetroGate.UI/lib/app_module.dart
@@ -1,11 +1,57 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'modules/game/game_module.dart';
 import 'modules/config/config_module.dart';
+import 'modules/installer/domain/repositories/installer_repository.dart';
+import 'modules/installer/domain/usecases/install_game_usecase.dart';
+import 'modules/installer/domain/usecases/cancel_installation_usecase.dart';
+import 'modules/installer/domain/usecases/delete_game_usecase.dart';
+import 'modules/installer/domain/usecases/subscribe_to_progress_usecase.dart';
+import 'modules/installer/infra/datasources/installer_grpc_datasource.dart';
+import 'modules/installer/infra/repositories/installer_repository_impl.dart';
+import 'modules/installer/presentation/bloc/installer_bloc.dart';
 
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    // Global bindings can be added here
+    // Global Installer DataSource
+    i.addLazySingleton<InstallerGrpcDataSource>(
+      () => InstallerGrpcDataSource(
+        host: 'localhost',
+        port: 5000,
+      ),
+    );
+
+    // Global Installer Repository
+    i.addLazySingleton<InstallerRepository>(
+      () => InstallerRepositoryImpl(i.get<InstallerGrpcDataSource>()),
+    );
+
+    // Global Installer UseCases
+    i.addLazySingleton<SubscribeToProgressUseCase>(
+      () => SubscribeToProgressUseCase(i.get<InstallerRepository>()),
+    );
+    
+    i.addLazySingleton<InstallGameUseCase>(
+      () => InstallGameUseCase(i.get<InstallerRepository>()),
+    );
+    
+    i.addLazySingleton<CancelInstallationUseCase>(
+      () => CancelInstallationUseCase(i.get<InstallerRepository>()),
+    );
+    
+    i.addLazySingleton<DeleteGameUseCase>(
+      () => DeleteGameUseCase(i.get<InstallerRepository>()),
+    );
+
+    // Global InstallerBloc (singleton - starts on app init)
+    i.addSingleton<InstallerBloc>(
+      () => InstallerBloc(
+        subscribeToProgressUseCase: i.get<SubscribeToProgressUseCase>(),
+        installGameUseCase: i.get<InstallGameUseCase>(),
+        cancelInstallationUseCase: i.get<CancelInstallationUseCase>(),
+        deleteGameUseCase: i.get<DeleteGameUseCase>(),
+      ),
+    );
   }
 
   @override
