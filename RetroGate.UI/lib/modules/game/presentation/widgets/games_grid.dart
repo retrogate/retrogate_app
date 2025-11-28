f99fe2cb3ab364070932ac56558d4ac802ef5b96
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-28 15:53:22 -0300

    feat: remove isPending parameter from GamesGrid widget to streamline game state handling

--- RetroGate.UI/lib/modules/game/presentation/widgets/games_grid.dart
+++ RetroGate.UI/lib/modules/game/presentation/widgets/games_grid.dart
@@ -104,11 +104,7 @@ class _GamesGridState extends State<GamesGrid> {
                   final progress = installerState is InstallerDataState
                       ? installerState.getProgress(game.id)
                       : null;
-                  
-                  final isPending = installerState is InstallerDataState
-                      ? installerState.isPending(game.id)
-                      : false;
-                  
+
                   if(progress != null) {
                     game.isInstalled = progress.state == InstallerProgressState.completed;
                   }
@@ -117,7 +113,6 @@ class _GamesGridState extends State<GamesGrid> {
                     game: game,
                     index: index,
                     installProgress: progress,
-                    isPending: isPending,
                     onTap: () {
                       if (widget.onGameSelected != null) {
                         widget.onGameSelected!(game, index);
