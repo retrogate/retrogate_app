1ca006148f9bddb635f4728992b34587fbe8bd8a
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-28 15:53:13 -0300

    feat: integrate pending installations retrieval in GetAllGamesUseCase

--- RetroGate.UI/lib/modules/game/domain/usecases/get_all_games_usecase.dart
+++ RetroGate.UI/lib/modules/game/domain/usecases/get_all_games_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:retrogate_ui/modules/game/domain/models/game_source.dart';
+import 'package:retrogate_ui/modules/installer/domain/usecases/get_pending_installations.dart';
 import '../models/game.dart';
 import '../repositories/available_games_repository.dart';
 import '../repositories/installed_games_repository.dart';
@@ -7,16 +8,19 @@ import '../repositories/installed_games_repository.dart';
 class GetAllGamesUseCase {
   final IAvailableGamesRepository availableGamesRepository;
   final IInstalledGamesRepository installedGamesRepository;
+  final GetPendingInstallations getPendingInstallations;
 
   GetAllGamesUseCase({
     required this.availableGamesRepository,
     required this.installedGamesRepository,
+    required this.getPendingInstallations,
   });
 
   Future<Either<Exception, List<Game>>> call(GameSource source) async {
     switch (source) {
       case GameSource.available:
         var installedGames = await installedGamesRepository.getAll(GameSource.installed);
+        var pendingInstallations = await getPendingInstallations();
         var availableGames = await availableGamesRepository.getAll(source);
         installedGames.fold(
           (l) {}, 
@@ -24,6 +28,9 @@ class GetAllGamesUseCase {
             for (var game in availableGames.getOrElse(() => [])) {
               if (r.any((installedGame) => installedGame.id == game.id)) {
                 game.isInstalled = true;
+              }
+              if (pendingInstallations.contains(game.id)) {
+                game.isPending = true;
               }
             }
           });
